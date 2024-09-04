9a4c2f0b791f251bd2f24810b60d2182806694cb
family <gnswp21@gmail.com>
2024-09-04 16:14:13 +0900

    Bugfix set total var for exact logging

--- extract/extract_kaggle.py
+++ extract/extract_kaggle.py
@@ -24,17 +24,17 @@ bucket_name = os.environ['BUCKET_NAME']
 
 dataset = os.environ['DATASET']
 files = kaggle_api.dataset_list_files(dataset).files
-
+total = len(files)
 
 # 파일 다운
-logging.info(f"Total {len(files)} files")
+logging.info(f"Total {total} files")
 for i, file in enumerate(files):
-    
-    logging.info(f"NOW {i+1}/{len(files)} file")
+
+    logging.info(f"NOW {i+1}/{total} file")
     file_name = file.name
     kaggle_api.dataset_download_file(dataset=dataset,
                                      file_name=file_name)
-    # 
+    # kaggle api는 파일의 크기가 크면 압축파일로 제공한다.
     file_name = file_name + '.zip'
     # 파일이 생성될 때까지 대기
     elapsed_time = 0
@@ -42,11 +42,11 @@ for i, file in enumerate(files):
     while elapsed_time < max_wait_time:
         # 현재 디렉토리의 파일 목록 가져오기
         files = os.listdir()
-        
+
         if file_name in files:
             logging.info(f"{file_name} is Found ")
             break
-    
+
         # 1분(60초) 대기
         time.sleep(60)
         elapsed_time += 60
@@ -54,8 +54,7 @@ for i, file in enumerate(files):
         if elapsed_time >= max_wait_time:
             logging.info(f" {file_name} is NOT Found until limit")
             break
-            
-    
+
     logging.info(f"Uploading {file_name} to S3...")
     s3.upload_file(file_name, bucket_name, file_name)
     # 파일 다운 후 삭제 메모리 용량보다 전체 파일의 합이 크기 때문
